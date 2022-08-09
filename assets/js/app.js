fc1f0702d85e86dcdef623a6b626979d49231b1b
UlviNuri <102859099+UlviNuri@users.noreply.github.com>
2022-08-09 19:13:24 +0400

    new

---
+++ assets/js/app.js
@@ -0,0 +1,15 @@
+var cursor = document.getElementById("cursor");
+document.body.addEventListener("mousemove", function(e) {
+  cursor.style.left = e.clientX + "px",
+    cursor.style.top = e.clientY + "px";
+});
+
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
